0beb04cd44db6394a5fd4166f039a58c7909844d
“username” <tjqdnwj@gmail.com>
2023-08-29 17:51:31 +0900

    [feat]develop: constants 정리

--- lib/constants.dart
+++ lib/constants.dart
@@ -17,11 +17,3 @@ const double radiusRoundButton = 60;
 const double radiusBottomNavi = 25;
 const double searcherBorderRadius = 8;
 const String fontStyleNanumBold = '나눔스퀘어-볼드';
-
-String medicineName = '타이레놀정 500mg';
-String ingredientDescription = 'It very veryvveryveryveryveryveryveryveryveryveryvery Coool';
-String infor = '사용법 DummyFile';
-//정보 맨위에있는
-String dummy =
-    '만 12세이상의 소아 및 성인일 경우: 1회 1~2정씩 1일 3~4회 (4-6시간 마다) 필요시 복용합니다.타이레놀500은 가능한 최단기간동안 최소유효용량으로 복용해야 합니다. ';
-const String medicineExplain = '아세트아미노펜, 단일성분, 무카페인, 무 색소의 빠르고 마음편한 해열진통제';
